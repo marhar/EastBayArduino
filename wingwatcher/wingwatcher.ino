04cbd98994a7013c987b7ac30c3ab345cfdcc654
Mark Harrison <marhar@gmail.com>
2020-05-22 20:34:40 -0700

    Adding kalman filtering.  Copied files here to reduce dependencies.

--- wingwatcher/wingwatcher.ino
+++ wingwatcher/wingwatcher.ino
@@ -1,8 +1,9 @@
 #include "ArrbotMonitor.h"
+#include "kalman.h"
 
 // attach:
 //   servo lead: - to gnd, + to Vin, signal to D2
-//   speaker:  gnd, D8
+//   8 Ohm speaker:  gnd, D8
 
 
 // Interrupt 0 services digital pin 2.
@@ -17,10 +18,14 @@ void setup() {
   attachInterrupt(PWM_INTERRUPT, pulseHandler, CHANGE);
 }
 
+Kalman1d pulseFilter(2, 2, 0.01);
 void loop() {
+    int smoothedPulseWidth = pulseFilter.updateEstimate(pulseWidth);
     MONITOR(pulseWidth);
+    MONITOR(smoothedPulseWidth);
     MONITOR_ENDL();
-    int pitch = map(pulseWidth, 900, 2100, 110, 110*8);
+
+    int pitch = map(smoothedPulseWidth, 900, 2100, 110, 110*8);
     tone(8, pitch);
     delay(10);
 }
