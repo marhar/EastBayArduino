6e44082813f96e5aa446da7fa3dc97dcb9c68d66
Mark Harrison <marhar@gmail.com>
2020-06-13 21:04:26 -0700

    Working with 4S and with duty cyle.

--- lipoVoltage/lipoVoltage.ino
+++ lipoVoltage/lipoVoltage.ino
@@ -1,25 +1,42 @@
-// Measure the voltage of a single LiPo cell.
-#include "ArrbotMonitor.h"
+#include <SimpleKalmanFilter.h>
+#include <FrSkySportSensorFlvss.h>
+#include <FrSkySportTelemetry.h>
+
+#define SPORT_PIN FrSkySportSingleWireSerial::SOFT_SERIAL_PIN_2
+#define MAX_VOLTAGE 5.0
+#define NSTEPS 1023
+SimpleKalmanFilter voltageFilter(3.7, 2, .5);
+const int voltagePin = A0;
+
+
+FrSkySportSensorFlvss flvss;
+FrSkySportTelemetry telemetry;
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  telemetry.begin(SPORT_PIN, &flvss);  
 }
+
+int ledState;
+uint32_t nextFlash;
+
+#define HAS_4_CELLS 1
 
 void loop() {
-  singleCell();
-  delay(1000);
-}
-
-// 4.2v is highest standard lipo cell voltage
-// range of 0% - 100% is 3.3v to 4.2v
-int highestReading = 1024 * 4.2/5.0;
-
-void singleCell() {
-  int x = analogRead(A0);
-  int milliVolts = map(x, 0, highestReading, 0, 4200);
-  int percent = map(milliVolts, 3300, 4200, 0, 100);
-  MONITOR(x);
-  MONITOR2("voltage", milliVolts/1000.0);
-  MONITOR(percent);
-  MONITOR_ENDL();
+  float rawVoltage = analogRead(voltagePin)*(MAX_VOLTAGE/NSTEPS);
+  float voltage = voltageFilter.updateEstimate(rawVoltage);
+  if (HAS_4_CELLS)
+    flvss.setData(voltage, voltage, voltage, voltage);
+  telemetry.send();
+  uint32_t now = millis();
+  if (now > nextFlash) {
+    if (voltage < 3.3)
+      nextFlash = now + 30;
+    else {
+      int ontime = map(voltage*100, 330, 420, 10, 990);
+      nextFlash = now + (ledState ? ontime : 1000 - ontime) * 2;
+    }
+    digitalWrite(LED_BUILTIN, ledState);
+    ledState = !ledState;
+  }
 }
